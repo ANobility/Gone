ab88921e99a5d3e048c01befd69a05379f3d1e1c
weber <weber@ZHAOWEIBOdeMacBook-Air.local>
2023-04-03 11:22:47 +0800

    testJSON

--- main.go
+++ main.go
@@ -5,14 +5,23 @@ import (
 	"net/http"
 )
 
+type myForm struct {
+	Colors []string `form:"colors[]"`
+}
+
 func main() {
 	r := gin.Default()
-	r.LoadHTMLGlob("./templates/*")
-	r.GET("/demo", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"name": "admin",
-			"pwd":  "112345",
-		})
+
+	r.GET("/someJSON", func(c *gin.Context) {
+		data := map[string]interface{}{
+			"lang": "GO语言",
+			"tag":  "<br>",
+		}
+
+		// 输出 : {"lang":"GO\u8bed\u8a00","tag":"\u003cbr\u003e"}
+		c.AsciiJSON(http.StatusOK, data)
 	})
-	r.Run()
+
+	// 监听并在 0.0.0.0:8080 上启动服务
+	r.Run(":8080")
 }
